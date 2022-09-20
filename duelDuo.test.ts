b80b9f4e5aac8af8baec2335a41fda63ec200a74
Rebecca Webb <rebeccawebb817@gmail.com>
2022-09-19 22:53:12 -0500

    before Heroku changes

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -18,3 +18,24 @@ test('Title shows up when page loads', async () => {
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
 })
+
+test('Draw Button displays choices', async () =>{
+    const drawBtn = await driver.findElement(By.id('draw'))
+    drawBtn.click();
+    const choiceDiv = await driver.findElement(By.id('choices'))
+    await driver.sleep(2000)
+    const displayed = await choiceDiv.isDisplayed();
+    expect(displayed).toBe(true);
+})
+
+test('Display Player-duo when "add to duo" button is clicked', async () =>{
+    const drawBtn = await driver.findElement(By.id('draw'))
+    drawBtn.click();
+    await driver.sleep(2000)
+    const duoBtn = await driver.findElement(By.xpath(`//*[@id="choices"]/div[1]/button`))
+    duoBtn.click();
+    const playerDiv = await driver.findElement(By.id('your-duo-header'))
+    await driver.sleep(2000)
+    const displayed = await playerDiv.isDisplayed();
+    expect(displayed).toBe(true);
+})
--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,14 @@
 const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
-    // CODE HERE
+    const testArr = [1,2,3,4,5]
+
+    test('Shuffled Array returns an array', () => {expect(Array.isArray(shuffleArray(testArr))).toBe(true)
+})
+
+//test('Array test tests arrays', () => {expect(Array.isArray(1)).toBe(true);})
+
+    // test("Shuffled Array is the correct length", () => {
+    //     expect(shuffleArray(testArr).length).toEqual(testArr.length);
+    // })
 })
